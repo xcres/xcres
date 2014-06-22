9eaf00c1eaf17388423c653d8f3bfbdf43b37b9d
Marius Rackwitz <git@mariusrackwitz.de>
2014-06-22 18:52:16 +0200

    Added option to Logger to disable coloring

--- lib/xcresources/logger.rb
+++ lib/xcresources/logger.rb
@@ -1,8 +1,13 @@
 class XCResources::Logger
 
-  attr_accessor :verbose, :silent
+  attr_accessor :verbose, :silent, :colored
   alias :verbose? verbose
   alias :silent? silent
+  alias :colored? colored
+
+  def initialize
+    self.colored = true
+  end
 
   def inform message, *format_args
     puts message % format_args unless silent?
@@ -10,9 +15,12 @@ class XCResources::Logger
 
   # Print arguments bold
   def inform_colored message, color, *format_args
-    # TODO: Test e.g: 'a %s b %10.00d c %1d d' => ["a %s", " b %10.00d", " c %1d", " d"]
-    message = message.gsub(/%\d*\.?\d*[a-z]/, "\0"+'\0'+"\0").split("\0").map(&color).reduce('', :+)
-    inform message, *format_args.map(&:to_s).map(&color).map(&:bold)
+    if colored?
+      # TODO: Test e.g: 'a %s b %10.00d c %1d d' => ["a %s", " b %10.00d", " c %1d", " d"]
+      message = message.gsub(/%\d*\.?\d*[a-z]/, "\0"+'\0'+"\0").split("\0").map(&color).reduce('', :+)
+      format_args = format_args.map(&:to_s).map(&color).map(&:bold)
+    end
+    inform message, *format_args
   end
 
   def log message, *format_args
