a82675fe12583bf41f6f3c68d3b0e9983a762717
Marius Rackwitz <git@mariusrackwitz.de>
2014-08-14 11:59:16 +0200

    [Spec] Added some first specs for ResourcesBuilder

--- spec/unit/builder/resources_builder_spec.rb
+++ spec/unit/builder/resources_builder_spec.rb
@@ -2,4 +2,31 @@ require File.expand_path('../../spec_helper', __FILE__)
 
 describe 'XCResources::ResourcesBuilder' do
 
+  def subject
+    XCResources::ResourcesBuilder
+  end
+
+  before do
+    @builder = subject.new
+    @builder.logger = stub('Logger')
+  end
+
+  describe "#initialize" do
+    it 'should set attribute documented to true' do
+      @builder.documented.should.be.true?
+    end
+  end
+
+  describe "#resources_constant_name" do
+    it 'should use the configured resources constant name' do
+      @builder.resources_constant_name = 'test'
+      @builder.resources_constant_name.should.be.eql?('test')
+    end
+
+    it 'should fallback to the basename of the output path' do
+      @builder.output_path = 'test/R.m'
+      @builder.resources_constant_name.should.be.eql?('R')
+    end
+  end
+
 end
