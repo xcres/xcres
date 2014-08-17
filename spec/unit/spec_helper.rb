077c96c6dd1a5e87ceea6408b50513aa2eae2f78
Marius Rackwitz <git@mariusrackwitz.de>
2014-08-17 10:14:51 +0200

    [Spec] Added helper function fixture_path

--- spec/unit/spec_helper.rb
+++ spec/unit/spec_helper.rb
@@ -2,3 +2,8 @@ require 'bacon'
 require 'pretty_bacon'
 require 'mocha-on-bacon'
 require 'xcresources'
+require 'pathname'
+
+def fixture_path
+  @fixture_path ||= Pathname(File.expand_path('../../fixtures', __FILE__))
+end
