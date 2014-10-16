8f6195bdce490eda80a8463c2afa903f2699d9f0
Marius Rackwitz <git@mariusrackwitz.de>
2014-10-16 15:38:31 +0200

    [Spec][Integration] Ensure a consistent alphabetical order for build settings

--- spec/spec_helper/xcodeproj_project_yaml.rb
+++ spec/spec_helper/xcodeproj_project_yaml.rb
@@ -12,6 +12,21 @@ class Xcodeproj::Project::Object::PBXShellScriptBuildPhase
   end
 end
 
+class Xcodeproj::Project::Object::XCBuildConfiguration
+  def pretty_print
+    data = {}
+    data['Build Settings'] = {}.tap do |sorted_settings|
+      build_settings.keys.sort.each do |key|
+        sorted_settings[key] = build_settings[key]
+      end
+    end
+    if base_configuration_reference
+      data['Base Configuration'] = base_configuration_reference.pretty_print
+    end
+    { name => data }
+  end
+end
+
 class Xcodeproj::Project
   def to_yaml
     pretty_print_output = pretty_print
